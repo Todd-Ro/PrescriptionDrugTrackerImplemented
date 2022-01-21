7100e2e71055a78e90d59bc28d1dc0e770f26fda
Todd Romkema <todd.romkema@gmail.com>
2022-01-21 01:54:19 -0600

    Add project files.

---
+++ PrescriptionDrugTracker/Controllers/PrescriptionController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrescriptionDrugTracker.Models;
+using System.Collections.Generic;
+
+namespace PrescriptionDrugTracker.Controllers
+{
+    public class PrescriptionController : Controller
+    {
+        List<Prescription> AllDrugs;
+
+        public IActionResult Index()
+        {
+            if(AllDrugs is null)
+            {
+                GenerateAllDrugs();
+            }
+            ViewBag.druglist = AllDrugs;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SelectMeds()
+        {
+            if (AllDrugs is null)
+            {
+                GenerateAllDrugs();
+            }
+            ViewBag.druglist = AllDrugs;
+            return View();
+        }
+
+        static List<Prescription> SelectedDrugs = new List<Prescription>();
+
+        [HttpPost]
+        public IActionResult MySelected(string drugname)
+        {
+            Prescription SelectedDrug = new Prescription(drugname,
+                Prescription.GetDrugLibrary()[drugname]);
+            if(!(SelectedDrugs.Contains(SelectedDrug)))
+            {
+                SelectedDrugs.Add(SelectedDrug);
+            }
+            ViewBag.mydruglist = SelectedDrugs;
+            return View();
+        }
+
+        List<Prescription> GenerateAllDrugs()
+        {
+            Dictionary<string, int> DrugsDict = Prescription.GetDrugLibrary();
+            List<Prescription> ret = new List<Prescription>();
+            foreach(string s in DrugsDict.Keys)
+            {
+                ret.Add(new Prescription(s, DrugsDict[s]));
+            }
+            AllDrugs = ret;
+            return ret;
+        }
+
+    }
+}
---
+++ PrescriptionDrugTracker/Models/Prescription.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrescriptionDrugTracker.Models
+{
+    public class Prescription
+    {
+        static Dictionary<string, int> DrugLibrary =
+            new Dictionary<string, int>()
+            {
+                {"Amoxicillin", 1 },
+                {"Penicillin", 1 },
+                {"Econazole", 3 },
+                {"Clonidine", 1 },
+                {"Azithromycin", 1 },
+                {"Lisinopril", 1 },
+                {"Propranolol", 1 },
+                {"Lovastatin", 1 },
+                {"Niacin", 3 },
+                {"Omega-3", 3 },
+                {"Provastatin", 1 }
+            };
+
+        public string DrugName { get; set; }
+        public int Tier {get; set;}
+        int Id;
+        static int NextId = 0;
+
+        static public Dictionary<string, int> GetDrugLibrary()
+        {
+            return DrugLibrary;
+        }
+
+        public Prescription(string drugName, int tier)
+        {
+            DrugName = drugName;
+            Tier = tier;
+            Id = NextId;
+            NextId++;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Prescription prescription &&
+                    DrugName == prescription.DrugName;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(DrugName);
+        }
+
+        
+
+
+    }
+}
