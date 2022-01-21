4e334e515826ec23b322e0550c7b5485f7b4814a
Todd Romkema <todd.romkema@gmail.com>
2022-01-21 03:37:04 -0600

    Got most functionality added

--- PrescriptionDrugTracker/Controllers/PrescriptionController.cs
+++ PrescriptionDrugTracker/Controllers/PrescriptionController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PrescriptionDrugTracker.Models;
+using PrescriptionDrugTrackerImplemented.Models;
+using System;
 using System.Collections.Generic;
 
 namespace PrescriptionDrugTracker.Controllers
@@ -31,16 +33,56 @@ namespace PrescriptionDrugTracker.Controllers
 
         static List<Prescription> SelectedDrugs = new List<Prescription>();
 
+        [HttpGet]
         [HttpPost]
         public IActionResult MySelected(string drugname)
         {
-            Prescription SelectedDrug = new Prescription(drugname,
-                Prescription.GetDrugLibrary()[drugname]);
-            if(!(SelectedDrugs.Contains(SelectedDrug)))
+            if(!(drugname is null))
             {
-                SelectedDrugs.Add(SelectedDrug);
+                Prescription SelectedDrug = new Prescription(drugname,
+                Prescription.GetDrugLibrary()[drugname]);
+                if (!(SelectedDrugs.Contains(SelectedDrug)))
+                {
+                    SelectedDrugs.Add(SelectedDrug);
+                }
             }
             ViewBag.mydruglist = SelectedDrugs;
+            return View();
+        }
+
+        static User DefaultUser = new User();
+        static User CurrentUser = DefaultUser;
+
+        [HttpGet]
+        [Route("/prescription/setexpiration/{drugname?}")]
+        public IActionResult SetExpiration(string drugname)
+        {
+            ViewBag.drugname = drugname;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/prescription/processsetexpiration/{drugname?}")]
+        public IActionResult ProcessSetExpiration(string drugname, string lastfilled, int daysoffill)
+        {
+            int year = int.Parse(lastfilled.Substring(0, 4));
+            int month = int.Parse(lastfilled.Substring(5, 2));
+            int day = int.Parse(lastfilled.Substring(8));
+            DateTime LastFillDate = new DateTime(year, month, day);
+            DateTime RefillDue = LastFillDate.AddDays(daysoffill);
+            if(!(CurrentUser.Expirations.ContainsKey(drugname)))
+            {
+                CurrentUser.Expirations.Add(drugname, RefillDue);
+            }
+            
+            return Redirect("/Prescription/MySelected");
+        }
+
+        public IActionResult ViewUserProfile()
+        {
+            ViewBag.usermeds = CurrentUser.GetUserMedsWithExpirations();
+            ViewBag.userexpiries = CurrentUser.GetUserMedExpirations();
+
             return View();
         }
 
@@ -55,6 +97,10 @@ namespace PrescriptionDrugTracker.Controllers
             AllDrugs = ret;
             return ret;
         }
+
+
+
+
 
     }
 }
---
+++ PrescriptionDrugTracker/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrescriptionDrugTrackerImplemented.Models
+{
+    public class User
+    {
+        public Dictionary<string, DateTime> Expirations { get; set; }
+
+        public User()
+        {
+            Expirations = new Dictionary<string, DateTime>();
+        }
+
+        public List<string> GetUserMedsWithExpirations()
+        {
+            return Expirations.Keys.ToList();
+        }
+
+        public List<string> GetUserMedExpirations()
+        {
+            List<string> meds = GetUserMedsWithExpirations();
+            List<string> ret = new List<string>();
+            foreach(string med in meds)
+            {
+                ret.Add(Expirations[med].ToString());
+            }
+            return ret;
+        }
+    }
+}
